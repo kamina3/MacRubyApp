b32f8f84d8bd2662aab1dffceb1b50ee941bfae9
neige7 <yamashitakazuki@neige436.local>
2012-02-16 22:26:59 +0900

    ボタンを押すと"the button was pressed"とだけ出るアプリケーション

---
+++ MacRubyApp/Controller.rb
@@ -0,0 +1,14 @@
+#
+#  Controller.rb
+#  MacRubyApp
+#
+#  Created by 山下 和輝 on 12/02/16.
+#  Copyright 2012年 筑波大学. All rights reserved.
+#
+
+
+class Controller
+    def button_pressed(sender)
+        puts "The button was pressed"
+    end
+end
